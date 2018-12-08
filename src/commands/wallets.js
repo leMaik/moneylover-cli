c0aa816c21fc2f55ee6c88079a80c7109ae391c4
leMaik <an@maik.tk>
2018-12-08 18:25:14 +0100

    Fix inaccurate total balance due to precision errors.

--- src/commands/wallets.js
+++ src/commands/wallets.js
@@ -5,6 +5,7 @@ module.exports.builder = (yargs) => yargs
 module.exports.handler = async (argv) => {
   const chalk = require('chalk')
   const Table = require('cli-table3')
+  const Decimal = require('decimal.js')
   const { getMoneyLover } = require('../util')
 
   const ml = await getMoneyLover()
@@ -28,14 +29,17 @@ module.exports.handler = async (argv) => {
     if (!wallet.exclude_total) {
       const balances = Object.keys(wallet.balance[0])
       const balance = wallet.balance[0][balances[0]]
-      totals[balances[0]] = (totals[balances[0]] || 0) + parseFloat(balance)
+      if (!totals[balances[0]]) {
+        totals[balances[0]] = new Decimal(0)
+      }
+      totals[balances[0]] = totals[balances[0]].plus(new Decimal(balance))
     }
   }
   for (const balance of Object.keys(totals)) {
     table.push([
       '',
       balance,
-      { hAlign: 'right', content: totals[balance] }
+      { hAlign: 'right', content: totals[balance].toString() }
     ])
   }
 
